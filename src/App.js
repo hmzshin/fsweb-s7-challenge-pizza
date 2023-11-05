75e84bc74ab818db7baa7c7f2463019a67f2d732
hmzshin <hamzasahin@outlook.com>
2023-11-05 15:00:28 +0300

    homepage design is completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import HomePage from "./pages/HomePage";
+import "./App.css";
 
 const App = () => {
   return (
     <>
-      <h1>Teknolojik Yemekler</h1>
-      <p>Burdaki kodu silip kendi headerınızı ekleyebilirsiniz</p>
+      <HomePage />
     </>
   );
 };
---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import pizza from "../assets/mvp-banner.png";
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <header>
+        <h1>TEKNOLOJİK YEMEKLER</h1>
+      </header>
+      <section>
+        <p>KOD ACIKTIRIR PİZZA, DOYURUR</p>
+        <button>ACIKTIM</button>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
