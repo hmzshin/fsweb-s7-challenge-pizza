2961318097b291734e28a894562f35c2136b38d2
hmzshin <hamzasahin@outlook.com>
2023-11-08 16:10:06 +0300

    create html schema for adv-homepage

---
+++ src/components/Footer.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <div>
+          <section>
+            <h2>Teknolojik Yemekler</h2>
+            <p>
+              <img /> 341 Londonderry Road,
+              <br />
+              Istanbul Türkiye
+            </p>
+            <p>
+              <img />
+              acitim@teknolojikyemekler.com
+            </p>
+            <p>
+              <img />
+              +902161234567
+            </p>
+          </section>
+          <section>
+            <h4>Sıccacık Menuler</h4>
+            <p>Terminal Pizza</p>
+            <p>5 Kişilik Hackathlon Pizza</p>
+            <p>useEffect Tavuklu Pizza</p>
+            <p>Beyaz Console Frosty</p>
+            <p>Testler Geçti Mutlu Burger</p>
+            <p>Position Absolute Acı Burger</p>
+          </section>
+        </div>
+        <div>
+          <p></p>
+          <div>
+            <img />
+            <img />
+            <img />
+            <img />
+            <img />
+            <img />
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>2023 Teknolojik Yemekler</p>
+        <img />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,23 +1,147 @@
 import React from "react";
 import "./HomePage.css";
+import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
+import optionIcon1 from "../assets/adv-aseets/icons/1.svg";
+import optionIcon2 from "../assets/adv-aseets/icons/2.svg";
+import optionIcon3 from "../assets/adv-aseets/icons/3.svg";
+import optionIcon4 from "../assets/adv-aseets/icons/4.svg";
+import optionIcon5 from "../assets/adv-aseets/icons/5.svg";
+import optionIcon6 from "../assets/adv-aseets/icons/6.svg";
 
 const HomePage = () => {
   return (
     <div className="homePage">
-      <header>
-        <h1>Teknolojik Yemekler</h1>
-        {/* <img src={logo} /> */}
-      </header>
-      <section>
-        <p>
-          KOD ACIKTIRIR <br /> PİZZA, DOYURUR
-        </p>
-        <button data-cy="to-orderpage-button">
-          <Link to="/order">ACIKTIM</Link>
-        </button>
+      <section className="hero">
+        <header>
+          <h1>Teknolojik Yemekler</h1>
+          {/* <img src={logo} /> */}
+        </header>
+        <section>
+          <p>
+            KOD ACIKTIRIR <br /> PİZZA, DOYURUR
+          </p>
+          <button data-cy="to-orderpage-button">
+            <Link to="/order">ACIKTIM</Link>
+          </button>
+        </section>
+
+        <section></section>
       </section>
+      <section className="options">
+        <ul>
+          <li>
+            <img src={optionIcon1} />
+            <Link to="/">YENİ!Kore</Link>
+          </li>
+          <li>
+            <img src={optionIcon2} />
+            <Link to="/">Pizza</Link>
+          </li>
+          <li>
+            <img src={optionIcon3} />
+            <Link to="/">Burger</Link>
+          </li>
+          <li>
+            <img src={optionIcon4} />
+            <Link to="/">Kızartmalar</Link>
+          </li>
+          <li>
+            <img src={optionIcon5} />
+            <Link to="/">Fast food</Link>
+          </li>
+          <li>
+            <img src={optionIcon6} />
+            <Link to="/">Gazlı İçecekler</Link>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <div>
+          <div>
+            <h3>
+              Özel <br /> Lezzetus
+            </h3>
+            <p>Position:Absolute Acı Burger</p>
+            <button>SİPARİŞ VER</button>
+          </div>
+          <div>
+            <h4>
+              Hackathlon
+              <br />
+              Burger Menü
+            </h4>
+            <button>SİPARİŞ VER</button>
+          </div>
+          <div>
+            <h4>
+              <span>Çoooook</span>hızlı <br />
+              npm gibi kurye
+            </h4>
+            <button>SİPARİŞ VER</button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>en çok paketlenen menüler</p>
+            <p>Acıktıran Kodlara Doyuran LEzzetler</p>
+            <nav>
+              <ul>
+                <li>
+                  <Link>Ramen</Link>
+                </li>
+                <li>
+                  <Link>Pizza</Link>
+                </li>
+                <li>
+                  <Link>Burger</Link>
+                </li>
+                <li>
+                  <Link>French fries</Link>
+                </li>
+                <li>
+                  <Link>Fast food</Link>
+                </li>
+                <li>
+                  <Link>Sosft drinks</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div>
+            <div>
+              <img />
+              <p>Terminal Pizza</p>
+              <p>
+                <span>4.9</span>
+                <span>(200)</span>
+                <span>60₺</span>
+              </p>
+            </div>
+            <div>
+              <img />
+              <p>Position Absolute Acı Pizza</p>
+              <p>
+                <span>4.9</span>
+                <span>(928)</span>
+                <span>85₺</span>
+              </p>
+            </div>
+
+            <div>
+              <img />
+              <p></p>
+              <p>
+                <span>4.9</span>
+                <span>(462)</span>
+                <span>75₺</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </div>
   );
 };
